350aa15d5bb19fc6c641d409b6c4258efa60f2f5
Rong Yuan <orangechicken@Rongs-macbook.local>
2019-01-27 23:21:55 -0800

    Added build files.

---
+++ build/CLM/CMakeFiles/CLM.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/orangechicken/Documents/Developer/CFLT/CLM/src/CCNF_patch_expert.cpp" "/Users/orangechicken/Documents/Developer/CFLT/build/CLM/CMakeFiles/CLM.dir/src/CCNF_patch_expert.cpp.o"
+  "/Users/orangechicken/Documents/Developer/CFLT/CLM/src/CLM.cpp" "/Users/orangechicken/Documents/Developer/CFLT/build/CLM/CMakeFiles/CLM.dir/src/CLM.cpp.o"
+  "/Users/orangechicken/Documents/Developer/CFLT/CLM/src/CLMParameters.cpp" "/Users/orangechicken/Documents/Developer/CFLT/build/CLM/CMakeFiles/CLM.dir/src/CLMParameters.cpp.o"
+  "/Users/orangechicken/Documents/Developer/CFLT/CLM/src/CLM_utils.cpp" "/Users/orangechicken/Documents/Developer/CFLT/build/CLM/CMakeFiles/CLM.dir/src/CLM_utils.cpp.o"
+  "/Users/orangechicken/Documents/Developer/CFLT/CLM/src/LandmarkLocaliser.cpp" "/Users/orangechicken/Documents/Developer/CFLT/build/CLM/CMakeFiles/CLM.dir/src/LandmarkLocaliser.cpp.o"
+  "/Users/orangechicken/Documents/Developer/CFLT/CLM/src/LandmarkMapper.cpp" "/Users/orangechicken/Documents/Developer/CFLT/build/CLM/CMakeFiles/CLM.dir/src/LandmarkMapper.cpp.o"
+  "/Users/orangechicken/Documents/Developer/CFLT/CLM/src/PDM.cpp" "/Users/orangechicken/Documents/Developer/CFLT/build/CLM/CMakeFiles/CLM.dir/src/PDM.cpp.o"
+  "/Users/orangechicken/Documents/Developer/CFLT/CLM/src/PatchExperts.cpp" "/Users/orangechicken/Documents/Developer/CFLT/build/CLM/CMakeFiles/CLM.dir/src/PatchExperts.cpp.o"
+  "/Users/orangechicken/Documents/Developer/CFLT/CLM/src/SVR_patch_expert.cpp" "/Users/orangechicken/Documents/Developer/CFLT/build/CLM/CMakeFiles/CLM.dir/src/SVR_patch_expert.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../HyperLandmarks/src/include"
+  "../CLM/include"
+  "../CLM/src"
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/CLMFaceTest.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/orangechicken/Documents/Developer/CFLT/CLMFaceTest.cpp" "/Users/orangechicken/Documents/Developer/CFLT/build/CMakeFiles/CLMFaceTest.dir/CLMFaceTest.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../HyperLandmarks/src/include"
+  "../CLM/include"
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/orangechicken/Documents/Developer/CFLT/build/HyperLandmarks/CMakeFiles/HyperLandmarks.dir/DependInfo.cmake"
+  "/Users/orangechicken/Documents/Developer/CFLT/build/CLM/CMakeFiles/CLM.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
